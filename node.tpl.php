784441c6405525cc7af986a9126489656751312b
Kalman Hosszu <hosszu.kalman@gmail.com>
2011-01-24 06:46:51 +0100

    Design meta data (links, submitted and terms)

--- node.tpl.php
+++ node.tpl.php
@@ -50,25 +50,25 @@
 ?>
 <div id="node-<?php print $node->nid; ?>" class="node<?php if ($sticky) { print ' sticky'; } ?><?php if (!$status) { print ' node-unpublished'; } ?> clear-block">
 
-<?php print $picture ?>
-
 <?php if (!$page): ?>
   <h2><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
 <?php endif; ?>
 
-  <div class="meta">
-  <?php if ($submitted): ?>
-    <span class="submitted"><?php print $submitted ?></span>
-  <?php endif; ?>
-
-  <?php if ($terms): ?>
-    <div class="terms terms-inline"><?php print $terms ?></div>
-  <?php endif;?>
-  </div>
 
   <div class="content">
     <?php print $content ?>
   </div>
 
-  <?php print $links; ?>
+  <div class="meta">
+    <?php if ($submitted): ?>
+      <div class="meta-row submitted b-s-b-ccc"><?php print $submitted ?></div>
+    <?php endif; ?>
+
+    <?php if ($terms): ?>
+      <div class="meta-row terms b-s-b-ccc"><?php print $terms ?></div>
+    <?php endif;?>
+
+    <div class="meta-row"><?php print $links; ?></div>
+  </div>
+
 </div>
